6270d4659a104e80b74bb810529cb96c7c98a659
zgreen123935 <z.green@me.com>
2025-01-24 11:49:34 -0330

    fix: Add specific allowed host for DigitalOcean

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,31 @@
 import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx()
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  
+  return {
+    plugins: [
+      vue(),
+      vueJsx()
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    server: {
+      port: 5173,
+      host: true
+    },
+    preview: {
+      port: 5173,
+      host: true,
+      allowedHosts: ['installer-app-j68mk.ondigitalocean.app']
     }
   }
 })
